df5878f4eb993148ba19872681b11109d70ed939
Vlad Serikov <vladikserikov@gmail.com>
2024-05-14 22:13:37 +0700

    Added workers

---
+++ test/fn.test.cjs
@@ -0,0 +1,11 @@
+const { fn } = require('../index.cjs');
+const { test, describe } = require('node:test');
+const assert = require('node:assert');
+
+describe('Test group', () => {
+    test('return 2', () => {
+        const result = fn();
+        assert.equal(result, 2);
+    });
+});
+
