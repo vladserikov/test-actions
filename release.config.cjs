d79aed0965b0d328ae25df4e427b57cbcac6ddbb
Vlad Serikov <vladikserikov@gmail.com>
2024-05-15 17:04:31 +0700

    added release

---
+++ release.config.cjs
@@ -0,0 +1,7 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+    branches: ['main', 'develop'],
+    repositoryUrl: 'https://github.com/vladserikov/test-actions.git',
+};
