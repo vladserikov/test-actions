b71884b4eb4014aac780347279faed8b331c27e3
Vlad Serikov <vladikserikov@gmail.com>
2024-05-15 17:49:55 +0700

    release job

--- release.config.cjs
+++ release.config.cjs
@@ -4,4 +4,13 @@
 module.exports = {
     branches: ['main', 'develop'],
     repositoryUrl: 'https://github.com/vladserikov/test-actions.git',
+    plugins: [
+        [
+            '@semantic-release/git',
+            {
+                assets: ['index.cjs', 'package.json'],
+                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
+    ],
 };
